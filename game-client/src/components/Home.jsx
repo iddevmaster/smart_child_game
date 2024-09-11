0c0561384f5623eae50513ab5ecf624cf888b911
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-09-11 19:48:42 +0700

    fx

--- game-client/src/components/Home.jsx
+++ game-client/src/components/Home.jsx
@@ -7,10 +7,16 @@ const Home = () => {
             <div className="position-relative h-100">
                 <div className="position-absolute top-50 start-50 translate-middle">
                     <div className="findfruitlogo row mb-5">
+                        <div className="text-center d-flex justify-content-center bounceIn1 fs-5 fw-bold" style={{ textShadow: '2px 2px 5px #fff' }}>
+                            <p>โครงการสื่ออัจฉริยะเพิ่มทักษะพัฒนาเด็กเล็ก</p>
+                        </div>
                         <div className="d-flex mx-5">
                             <p id="find" className="text-primary logotext1 slideInRight">Find</p>
                             <img src="/img/aa.png" alt="" className="mx-2 logoicon zoomIn" />
                             <p id="fruit" className="text-danger logotext2 slideInLeft">Fruit</p>
+                        </div>
+                        <div className="text-center d-flex justify-content-center bounceIn1" id="projectTitle">
+                            <p>Smart Child Media Development Project</p>
                         </div>
                     </div>
                     <div className="row mb-4 bounceIn1">
